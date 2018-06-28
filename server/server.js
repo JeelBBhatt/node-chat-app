82296c98ef12996227dee0b80d7188efecbd1027
JeelBBhatt <lanetteam.jeel@gmail.com>
2018-06-28 18:17:17 +0530

    newmesage initial

--- server/server.js
+++ server/server.js
@@ -1,11 +1,41 @@
 const path =require('path');
 const express=require('express');
+const http=require('http');
+const socketIO=require('socket.io');
 
 const publicpath=path.join(__dirname,'../public');
 const port=process.env.PORT || 3000;
+
 var app=express();
+var server=http.createServer(app);
+var io=socketIO(server);
+
+io.on('connection',(socket)=>{
+	console.log("New User connected");
+
+	// socket.emit('newEmail',{
+	// 	from: "jeel1232gmail.com",
+	// 	text: "Hello , How r u??",
+	// 	createdAt: 145
+	// });
+	// socket.on('createEmail',(newEmail)=>{
+	// 	console.log("Created Email",newEmail);		
+	// });
+	socket.emit('newMessage',{
+		from: "rina",
+		text: "Hello , How r u??",
+		createdAt: 145
+	});
+
+	socket.on('createMessage',(messsage)=>{
+	console.log("Created Mesage",messsage);		
+	});
+	socket.on('disconnect',()=>{
+	console.log("User was disconnected");		
+	});
+});
 
 app.use(express.static(publicpath));
-app.listen(port,()=>{
-	console.log(`server strated on ${port} 3000`);
+server.listen(port,()=>{
+	console.log(`server strated on ${port}`);
 });
